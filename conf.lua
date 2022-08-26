c333fbbcd9bddd5801a1cd62106f4794b6037f42
isums <78246099+isums@users.noreply.github.com>
2022-08-26 14:21:09 +0800

    Delete conf.lua

--- conf.lua
+++
@@ -1,6 +0,0 @@
-function love.conf(t)
-    t.title = "The Adventures of Pudding"                  -- The title of the window the game is in (string)
-    t.version = "11.4"                    -- The LÖVE version this game was made for (string)
-    t.window.width = 1280                 -- The window width (number)
-    t.window.height = 720                 -- The window height (number)
-end
